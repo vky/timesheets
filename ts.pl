1627641146e540bad09f2b0f74111e9d28062cfc
Vijay Korapaty <vijay.korapaty@gmail.com>
2013-02-16 16:30:02 -0800

    Initial commit.

---
+++ ts.pl
@@ -0,0 +1,20 @@
+use strict;
+use warnings;
+use Data::Dumper;
+
+
+
+__END__
+=idea
+
+start
+end
+lunch start
+lunch end
+report (will show information for current week)
+report start_date end_date (show information for time inbetween given days)
+report export (will export the last week)
+
+
+=cut
+
